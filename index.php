9d88100600ebb8db5cb2cf558d2514a7d14021ab
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-11-05 08:11:24 +0200

    .

--- index.php
+++ index.php
@@ -5,13 +5,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="content/css/bootstrap.css">
-    <link rel="stylesheet" href="content/css/styles.css">
+    <link rel="stylesheet" href="resources/css/bootstrap.css">
+    <link rel="stylesheet" href="resources/css/styles.css">
     <title>Sigma Clone</title>
 </head>
 
 <body>
-    <?php echo "test" ?>
+    <div class="product-image-container second_img">
+        <a href="item?id=4534&amp;name=NOTEBOOK-LENOVO-CI5-LEGION_5-(82JH006WAX)_I5-11400H_(6C/12T)-RAM_16G_(2*8)-SSD_1T_Gen4+(M.2)-RTX3060_6G_130w-15.6-FHD-IP"
+            target="_self"
+            title="NOTEBOOK-LENOVO-CI5-LEGION 5-(82JH006WAX) I5-11400H (6C/12T)-RAM 16G (2*8)-SSD 1T Gen4+(M.2)-RTX3060 6G 130w-15.6-FHD-IP">
+            <img src="resources/img/a.webp" id="img_4534" class="img-1 img-responsive" alt="image1">
+
+            <img src="resources/img/a.webp" id="img_254792" class="img-2 img-responsive" alt="image2">
+        </a>
+    </div>
+    <div class="caption hide-cont">
+
+        <h4><a href="item?id=4534&amp;name=NOTEBOOK-LENOVO-CI5-LEGION_5-(82JH006WAX)_I5-11400H_(6C/12T)-RAM_16G_(2*8)-SSD_1T_Gen4+(M.2)-RTX3060_6G_130w-15.6-FHD-IP"
+                title="NOTEBOOK-LENOVO-CI5-LEGION 5-(82JH006WAX) I5-11400H (6C/12T)-RAM 16G (2*8)-SSD 1T Gen4+(M.2)-RTX3060 6G 130w-15.6-FHD-IP"
+                target="_self">NOTEBOOK-LENOVO-CI5-LEGION 5-(82JH006WAX) I5-11400H (6C/12T)-RAM 16G (2*8)-SSD 1T
+                Gen4+(M.2)-RTX3060 6G 130w-15.6-FHD-IP</a></h4>
+
+    </div>
 </body>
 
 </html>
