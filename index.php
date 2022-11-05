0244777f232e66e9545009dc09eee418af95659a
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-11-05 08:30:10 +0200

    .

--- index.php
+++ index.php
@@ -11,13 +11,11 @@
 </head>
 
 <body>
-    <?php echo "Hona Yarkood Al 3adol" ?>
     <div class="second_img">
         <a href="item?id=4534&amp;name=NOTEBOOK-LENOVO-CI5-LEGION_5-(82JH006WAX)_I5-11400H_(6C/12T)-RAM_16G_(2*8)-SSD_1T_Gen4+(M.2)-RTX3060_6G_130w-15.6-FHD-IP"
             target="_self"
             title="NOTEBOOK-LENOVO-CI5-LEGION 5-(82JH006WAX) I5-11400H (6C/12T)-RAM 16G (2*8)-SSD 1T Gen4+(M.2)-RTX3060 6G 130w-15.6-FHD-IP">
             <img src="resources/img/a.webp" class="img-1 img-responsive" alt="image1">
-
             <img src="resources/img/a.webp" class="img-2 img-responsive" alt="image2">
         </a>
     </div>
