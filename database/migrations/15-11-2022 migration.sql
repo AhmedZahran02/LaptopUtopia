b6d1372c50d01eee76da7033e65a2a8d127b1de2
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-11-15 11:22:39 +0200

    alot of updates in sql migration

--- database/migrations/15-11-2022 migration.sql
+++ database/migrations/15-11-2022 migration.sql
@@ -21,5 +21,65 @@ IF NOT EXISTS `brand`
   FOREIGN KEY(WorkField) REFERENCES WorkField(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE
+IF NOT EXISTS `customer`
+(
+  `username` varchar(50) NOT NULL,
+  `firstname` varchar (255) DEFAULT NULL,
+  `lastname` varchar (255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar (255) NOT NULL,
+  `phone` varchar (255) DEFAULT NULL,
+  `dateofbirth` DATE DEFAULT NULL,
+  `city` varchar (255) DEFAULT NULL,
+  `street` varchar (255) DEFAULT NULL,
+  `housenumber` int DEFAULT NULL,
+  PRIMARY KEY (username)
+);
 
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE
+IF NOT EXISTS `admin`
+(
+  `username` varchar(50) NOT NULL,
+  `firstname` varchar (255) DEFAULT NULL,
+  `lastname` varchar (255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar (255) NOT NULL,
+  `phone` varchar (255) DEFAULT NULL,
+  `dateofbirth` DATE DEFAULT NULL,
+  `startedworkat` DATE DEFAULT NULL,
+  PRIMARY KEY (username)
+);
 
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE
+IF NOT EXISTS `product`
+(
+  `mid` varchar(50) NOT NULL,
+  `workfield` varchar (255) NOT NULL,
+  `body` varchar (500) DEFAULT NULL,
+  `title` varchar (255) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `imageurls` varchar (500) DEFAULT NULL,
+  PRIMARY KEY (mid),
+  FOREIGN KEY (workfield) REFERENCES WorkField(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS `cpu`;
+CREATE TABLE
+IF NOT EXISTS `cpu`
+(
+  `mid` varchar(50) NOT NULL,
+  `subbrand` varchar (255) NOT NULL,
+  `brandmodifier` varchar (255) NOT NULL,
+  `skunumber` varchar (255) NOT NULL,
+  `numberofcores` int NOT NULL,
+  `numberofthreads` int NOT NULL,
+  `clockspeed` int NOT NULL,
+  `cache` int NOT NULL,
+  PRIMARY KEY (mid),
+  FOREIGN KEY (mid) REFERENCES product(mid) ON DELETE CASCADE ON UPDATE CASCADE
+);
