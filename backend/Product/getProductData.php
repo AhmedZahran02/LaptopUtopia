c9151b84961ba94911b13bd155148ab3021832d4
Malek-2002 <89798604+Malek-2002@users.noreply.github.com>
2022-12-17 12:23:47 +0200

    Update getProductData.php

--- backend/Product/getProductData.php
+++ backend/Product/getProductData.php
@@ -8,15 +8,15 @@ if (isset($_GET['mid']) && isset($_GET['workfield'])) {
     $mydb->connect();
 
     if ($workField == "0") {
-        $query = "SELECT * FROM laptop , product WHERE product.mid = '" . $mid . "'" . "and cpu.mid = product.mid";
+        $query = "SELECT * FROM laptop , product WHERE product.mid = '" . $mid . "'" . "and laptop.mid = product.mid";
     } else if ($workField == "1") {
         $query = "SELECT * FROM cpu , product WHERE product.mid = '" . $mid . "'" . "and cpu.mid = product.mid";
     } else if ($workField == "2") {
-        $query = "SELECT * FROM gpu , product WHERE product.mid = '" . $mid . "'" . "and cpu.mid = product.mid";
+        $query = "SELECT * FROM gpu , product WHERE product.mid = '" . $mid . "'" . "and gpu.mid = product.mid";
     } else if ($workField == "3") {
-        $query = "SELECT * FROM ram , product WHERE product.mid = '" . $mid . "'" . "and cpu.mid = product.mid";
+        $query = "SELECT * FROM ram , product WHERE product.mid = '" . $mid . "'" . "and ram.mid = product.mid";
     } else if ($workField == "4") {
-        $query = "SELECT * FROM storage , product WHERE product.mid = '" . $mid . "'" . "and cpu.mid = product.mid";
+        $query = "SELECT * FROM storage , product WHERE product.mid = '" . $mid . "'" . "and storage.mid = product.mid";
     }
 
     //echo $query;
