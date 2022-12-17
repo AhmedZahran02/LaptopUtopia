2772cc80eee51101a8486a140b723b832fdb763c
Malek-2002 <89798604+Malek-2002@users.noreply.github.com>
2022-12-17 12:43:18 +0200

    Orders and signlogs count

---
+++ backend/Order/cancelOrder.php
@@ -0,0 +1,26 @@
+<?php
+include '../../php/dbclass.php';
+$mydb = new Database();
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $mydb->connect();
+
+    $query = "DELETE * FROM order WHERE id = ". $id . ";";
+    //echo $query;
+    $result = $mydb->query($query);
+
+    $respond = [];
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($respond, $row);
+    }
+
+    $respond = json_encode($respond);
+
+    echo $respond;
+    $mydb->freeResult();
+    $mydb->disconnect();
+
+    return $respond;
+}
