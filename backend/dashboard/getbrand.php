b2bab9525231d345d86e3947fd77ba137c9fce30
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-12-20 16:12:33 +0200

    .

---
+++ backend/dashboard/getbrand.php
@@ -0,0 +1,31 @@
+<?php
+include '../../php/dbclass.php';
+$mydb = new Database();
+
+$mydb->connect();
+
+$wf = isset($_GET['workfield']) ? $_GET['workfield'] : -1;
+
+if ($wf == -1) {
+    $mydb->disconnect();
+
+    return null;
+} else {
+    $query = "SELECT name FROM brand WHERE workfield = " . $wf . ";";
+    //echo $query;
+    $result = $mydb->query($query);
+
+    $respond = [];
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($respond, $row['name']);
+    }
+
+    $respond = json_encode($respond);
+
+    echo $respond;
+    $mydb->freeResult();
+    $mydb->disconnect();
+
+    return $respond;
+}
