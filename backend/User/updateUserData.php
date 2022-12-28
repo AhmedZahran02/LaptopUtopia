b1acc48866d528b31ef11f35f448b8433a448207
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-12-28 19:40:54 +0200

    .

--- backend/User/updateUserData.php
+++ backend/User/updateUserData.php
@@ -2,8 +2,8 @@
 include '../../php/dbclass.php';
 $mydb = new Database();
 
-if (isset($_POST['username'])) {
-
+if (isset($_POST['username']) && isset($_POST['isAdmin'])) {
+    $table = $_POST['isAdmin'] == 0 ? "customer" : "admin";
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $password = $_POST['password'];
@@ -11,8 +11,13 @@ if (isset($_POST['username'])) {
     $city = $_POST['city'];
     $street = $_POST['street'];
     $housenumber = $_POST['housenumber'];
+    $startedworkat = $_POST['startedworkat'];
 
-    $query = "update customer set firstname = '" . $username . "', lastname='" . $lastname . "', password='" . sha1($password) . "', phone='" . $phone . "', city= '" . $city . "', street='" . $street . "',housenumber=" . $housenumber . " where usename ='" . $username . "';";
+    if ($_POST['isAdmin'] == 0) {
+        $query = "update customer set firstname = '" . $firstname . "', lastname='" . $lastname . "', password='" . sha1($password) . "', phone='" . $phone . "', city= '" . $city . "', street='" . $street . "',housenumber=" . $housenumber . " where usename ='" . $username . "';";
+    } else {
+        $query = "update admin set firstname = '" . $username . "', lastname='" . $lastname . "', password='" . sha1($password) . "', phone='" . $phone . "', startedworkat='" . $startedworkat . " where usename ='" . $username . "';";
+    }
 
     $mydb->connect();
 
